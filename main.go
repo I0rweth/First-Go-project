ea3cf0a030cd7fd7a37655f71d1bc59538469ae6
Илья Макурин <hotingzmz@gmail.com>
2025-04-13 08:32:49 +0300

    Решение квадратного трехчлена

--- main.go
+++ main.go
@@ -1,9 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	fmt.Println("Hello,w")
-	var a string
+	var a float64
+	var b float64
+	var c float64
+
+	fmt.Println("РЕШИ квадратное уравнение")
+	fmt.Println("Введи а:")
 	fmt.Scan(&a)
+
+	fmt.Println("Введи b:")
+	fmt.Scan(&b)
+
+	fmt.Println("Введи c:")
+	fmt.Scan(&c)
+
+	D := (b * b) - 4*(a*c)
+
+	if D > 0 {
+		var x1 float64
+		var x2 float64
+
+		x1 = (-b + math.Sqrt(D)) / (2 * a)
+		x2 = (-b - math.Sqrt(D)) / (2 * a)
+		fmt.Println("2 корня\n D = " + fmt.Sprint(D))
+		fmt.Println("X1: " + fmt.Sprint(x1) + "\nX2: " + fmt.Sprint(x2))
+
+	} else if D == 0 {
+		var x float64
+
+		x = (-b) / (2 * a)
+		fmt.Println("один корень")
+		fmt.Println("X: " + fmt.Sprint(x))
+	} else if D < 0 {
+		fmt.Println("нет корней")
+	}
 }
